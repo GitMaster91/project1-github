28abc20e63a3431ead42c17d2748d99703604fee
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-01 00:03:19 -0600

    2 functions same index.js

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const take = n => n - 10;
 
 // from branch: develop1 Clone
 
+<<<<<<< Updated upstream
 // this function was created in develop1 clone
 
 function fn1(n) {
+=======
+
+// this function was created in develop1 origin
+
+function fn2(n) {
+>>>>>>> Stashed changes
   return n * 2;
 };
