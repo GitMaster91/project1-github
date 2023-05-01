e63404fccfc91af226be4a0e4df902e20c0580b9
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-01 00:09:42 -0600

    function deleted

--- index.js
+++ index.js
@@ -3,15 +3,4 @@ const take = n => n - 10;
 
 // from branch: develop1 Clone
 
-<<<<<<< Updated upstream
-// this function was created in develop1 clone
-
-function fn1(n) {
-=======
-
-// this function was created in develop1 origin
-
-function fn2(n) {
->>>>>>> Stashed changes
-  return n * 2;
-};
+// delete functions
