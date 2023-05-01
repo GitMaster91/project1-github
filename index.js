d8b6bce5cb598b896b3c94a8273b85c988c1a148
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-01 00:15:20 -0600

    Clean A

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const add  = n => n + 100;
-const take = n => n - 10;
-
-// from branch: develop1 Clone
-
-
-// this function was created in develop1 origin
-
-function fn2(n) {
-  return n * 5;
-};
+// Clean A
